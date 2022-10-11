af77e0d5ffec55bf975172bddcd787a61e14e59d
noelmandak <73637333+noelmandak@users.noreply.github.com>
2022-10-11 22:31:45 +0700

    initial commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template,request
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello():
+    name = request.args.get('name')
+
+    return render_template("index.html")
+
+@app.route('/register')
+def register():
+    first_name  = request.args.get('firstname')
+    last_name  = request.args.get('lastname')
+
+    full_name = first_name+last_name
+    return render_template("welcome.html",full_name=full_name)
+
+    
