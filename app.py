8fe153d98391506002166c0120768b12e3d19ffc
noelmandak <73637333+noelmandak@users.noreply.github.com>
2022-10-19 21:47:31 +0700

    buat rute dari login ke semua role

--- app.py
+++ app.py
@@ -3,66 +3,108 @@ from database import *
 import socket
 
 def role_verify(jabatan):
-    if session["username"] == "sales_admin": return True
+    if "jabatan" in session:
+        if session["jabatan"] == jabatan: return True
     return False
 
 
 @app.route('/')
 def home():
-    if "username" in session:
-        if session["username"] == "sales_admin":
+    if "jabatan" in session:
+        if session["jabatan"] == "sales_admin":
             return redirect(url_for("sales_admin"))
-        elif session["username"] == "finance_admin":
+        elif session["jabatan"] == "finance_admin":
             return redirect(url_for("finance_admin"))
-        elif session["username"] == "manager":
+        elif session["jabatan"] == "manager":
             return redirect(url_for("manager"))
         else:
-            flash("Akses ditolak")
+            flash("Akses ditolak, silahkan login dahulu")
             return redirect(url_for("logout"))
     else:
         return redirect(url_for("login"))
-    
+
+
+@app.route('/login',methods=['POST',"GET"])
+def login():
+    if "jabatan" in session:
+        if session["jabatan"] == "sales_admin":
+            return redirect(url_for("sales_admin"))
+        elif session["jabatan"] == "finance_admin":
+            return redirect(url_for("finance_admin"))
+        elif session["jabatan"] == "manager":
+            return redirect(url_for("manager"))
+
+    if request.method == 'POST':
+        curr_username  = request.form['username']
+        curr_password  = request.form['password']
+        curr_user = get_login_info(curr_username) #[username, password, jabatan]
+        if curr_user is not None:
+            if curr_user[1] == curr_password:
+                session['jabatan'] = curr_user[2]
+                session['username'] = curr_user[1]
+                if   session['jabatan'] == "sales_admin"  : return redirect(url_for("sales_admin"))
+                elif session['jabatan'] == "finance_admin": return redirect(url_for("finance_admin"))
+                elif session['jabatan'] == "manager"      : return redirect(url_for("manager"))
+            else:
+                flash("Gagal, username atau passord tidak cocok")
+                return redirect(url_for("login"))
+        else:
+            flash("Gagal, user tidak ditemukan")
+            return redirect(url_for("login"))
+    else:
+        return render_template("login.html")
+
+@app.route("/logout")
+def logout():
+    session.clear()
+    return redirect(url_for("login"))
+
+
+
+
+
+##### Sales Admin #####
 @app.route('/sales_admin')
 def sales_admin():
     if role_verify("sales_admin"):
         return render_template("sale.html")
+    flash("Akses ditolak")
+    return redirect(url_for("home"))
 
-    # if "username" in session:
-    #     if session["username"] == "sales_admin":
-    #         return render_template("sale.html")
-    #     else: 
-    #         flash("Akses ditolak")
-    #         return redirect(url_for("login"))
-    # flash("Login terlebih dahulu")
-    # return redirect(url_for("login"))
+
+
+
+
+
+##### Finance Admin #####
+@app.route('/finance_admin')
+def finance_admin():
+    if role_verify("finance_admin"):
+        return render_template("finance.html")
+    flash("Akses ditolak")
+    return redirect(url_for("home"))
+
+
+
+
+
+##### Manager #####
+@app.route('/manager')
+def manager():
+    if role_verify("manager"):
+        return render_template("manager.html")
+    flash("Akses ditolak")
+    return redirect(url_for("home"))
+
+
+
+
+
+
 
 @app.route('/new_invoice')
 def new_invoice():
     return render_template("new_invoice.html")
-
-@app.route('/finance_admin')
-def finance_admin():
-    # if "username" in session:
-    #     if session["username"] == "finance_admin":
-    #         return render_template("finance.html")
-    #     else: 
-    #         flash("Akses ditolak")
-    #         return redirect(url_for("login"))
-    # flash("Login terlebih dahulu")
-    # return redirect(url_for("login"))s
-    return render_template("finance.html")
-
-@app.route('/manager')
-def manager():
-    # if "username" in session:
-    #     if session["username"] == "manager":
-    #         return render_template("manager.html")
-    #     else: 
-    #         flash("Akses ditolak")
-    #         return redirect(url_for("login"))
-    # flash("Login terlebih dahulu")
-    # return redirect(url_for("login"))
-    return render_template("manager.html")
 
 @app.route("/popup")
 def popup():
@@ -84,39 +126,6 @@ def new_customer():
 def data_customer():
     return render_template("data_customer.html")
 
-@app.route('/login',methods=['POST',"GET"])
-def login():
-    if "username" in session:
-        if session["username"] == "sales_admin":
-            return redirect(url_for("sales_admin"))
-        elif session["username"] == "finance_admin":
-            return redirect(url_for("finance_admin"))
-        elif session["username"] == "manager":
-            return redirect(url_for("manager"))
-
-    if request.method == 'POST':
-        curr_username  = request.form['username']
-        curr_password  = request.form['password']
-        curr_user = get_login_info(curr_username) #[username, password, jabatan]
-        if curr_user is not None:
-            if curr_user[1] == curr_password:
-                session['username'] = curr_user[2]
-                if   session['username'] == "sales_admin"  : return redirect(url_for("sales_admin"))
-                elif session['username'] == "finance_admin": return redirect(url_for("finance_admin"))
-                elif session['username'] == "manager"      : return redirect(url_for("manager"))
-            else:
-                flash("Gagal, username atau passord tidak cocok")
-                return redirect(url_for("login"))
-        else:
-            flash("Gagal, user tidak ditemukan")
-            return redirect(url_for("login"))
-    else:
-        return render_template("login.html")
-
-@app.route("/logout")
-def logout():
-    session.clear()
-    return redirect(url_for("login"))
 
 if __name__ == '__main__':
     hostname = socket.gethostname()
